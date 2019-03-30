64d524734a5bfe527748da7b3f773669f10f11b1
Leandro Guina <guina86@gmail.com>
2019-03-30 18:13:54 -0300

    Consulta simples com query methods

--- src/main/java/com/leandroguina/workshopmongo/config/Instantiation.java
+++ src/main/java/com/leandroguina/workshopmongo/config/Instantiation.java
@@ -36,7 +36,7 @@ public class Instantiation implements CommandLineRunner{
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
 		Post post1 = new Post(null, LocalDate.of(2018, 03, 20), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, LocalDate.of(2018, 03, 22), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, LocalDate.of(2018, 03, 22), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
 		
 		CommentDTO c1 = new CommentDTO("Boa viagem mano!", LocalDate.of(2018,03,21), new AuthorDTO(alex));
 		CommentDTO c2 = new CommentDTO("Aproveite!", LocalDate.of(2018,03,21), new AuthorDTO(bob));
